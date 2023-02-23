fe42f6a90830eb9aff6942023fb4aaf8d42dce15
Fedenko <mfedenko@follett.com>
2023-02-23 16:48:28 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20230221-140725\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20230223-164738\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
